6e9ba86ae86633ab1868b3dad7e5b471300f0efe
Isaac <isaac.laster812@outlook.com>
2023-06-13 18:17:22 -0700

    google sso implemented

---
+++ client/src/App.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { GoogleLogin } from "@react-oauth/google";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <GoogleLogin
+          onSuccess={(credentialResponse) => {
+            console.log(credentialResponse);
+          }}
+          onError={() => {
+            console.log("Login Failed");
+          }}
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
